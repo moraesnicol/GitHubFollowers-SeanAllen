ff491e8119bac1bd1531e6e6f4114a2fa074d850
Gabriel <moraesnicol@gmail.com>
2022-01-27 18:20:38 -0300

    Initial Commit

---
+++ GitHubFollowers-SeanAllen/Features/FavoritesListVC.swift
@@ -0,0 +1,18 @@
+//
+//  FavoritesListVC.swift
+//  GitHubFollowers-SeanAllen
+//
+//  Created by Gabriel on 27/01/22.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+    
+
+}
---
+++ GitHubFollowers-SeanAllen/Features/SearchVC.swift
@@ -0,0 +1,19 @@
+//
+//  SearchVC.swift
+//  GitHubFollowers-SeanAllen
+//
+//  Created by Gabriel on 27/01/22.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+    
+
+}
